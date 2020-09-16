93d72cf2570246427ac0665d967545bcc7a756ea
Steven Bißantz <steven.bissantz@mail.de>
2020-09-16 09:05:17 +0200

    Built a constructor

---
+++ R/constructor.r
@@ -0,0 +1,30 @@
+# TODO Ordering of Arguments
+
+# Constructor -------------------------------------------------------------
+# new_muscldf(method = "disjoint", df = mtcars)
+
+new_muscldf <- function(x = list(), df = quote(), method = "disjoint",
+                        rit_min = double(), negative_too = FALSE, sclvals = NULL) {
+  stopifnot(exprs = {
+    is.list(x)
+    is.data.frame(df)
+    is.double(rit_min)
+    is.logical(negative_too)
+    is.vector(sclvals) || is.null(sclvals)
+    })
+  method <- match.arg(method, c("disjoint", "overlap"))
+  df <- match.call()$df
+
+  structure(x, class = "muscldf", method = method, rit_min = rit_min,
+            negative_too = negative_too, sclvals = sclvals, df = df)
+}
+
+
+
+
+
+
+
+
+
+
