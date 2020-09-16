b85da394d1ae92d4c1b1d60e4d8517768ec911a3
Steven Bißantz <steven.bissantz@mail.de>
2020-09-16 19:23:40 +0200

    Rebuild the construct

--- R/constructor.r
+++ R/constructor.r
@@ -2,19 +2,17 @@
 
 # Constructor -------------------------------------------------------------
 
-new_muscldf <- function(x = list(), df = quote(), method = "disjoint",
-                        rit_min = double(), negative_too = FALSE,
-                        sclvals = NULL) {
+new_muscldf <- function(x = list(), method = "disjoint",
+                        rit_min = NULL, negative_too = FALSE,
+                        sclvals = NULL, df = data.frame()) {
   stopifnot(exprs = {
     is.list(x)
-    is.data.frame(df)
-    is.double(rit_min)
+    is.data.frame(eval(df))
+    is.double(rit_min) || is.null(rit_min)
     is.logical(negative_too)
-    is.vector(sclvals) || is.null(sclvals)
+    is.vector(eval(sclvals)) || is.null(eval(sclvals))
     })
   method <- match.arg(method, c("disjoint", "overlap"))
-  df <- match.call()$df
-
   structure(x, class = "muscldf", method = method, rit_min = rit_min,
             negative_too = negative_too, sclvals = sclvals, df = df)
 }
