f2649cc43762e973efd9161f41dfaca253aaa5e1
Steven Bißantz <steven.bissantz@mail.de>
2020-09-17 23:38:37 +0200

    Messages only given detected problems

--- R/check_my.r
+++ R/check_my.r
@@ -1,44 +1,28 @@
 # TODO Use "!"for not, is FALSE for conditions
 # TODO Check if there is a linear relationship between the variables?
 # TODO WARNING should be errors?
+# use hasName
 
 check_my <- function(x){
-  if(!is.data.frame(x)){
-    stop("`x` is not a data frame. Scaling not possible", call. = FALSE)
-    }else{
-      message("`x` is a data frame.")
-      }
+  if(!is.data.frame(x))
+    stop("`x` is not a data.frame.", call. = FALSE)
   x_len <- length(x)
   if(x_len < 2){
-    stop("`x` has `< 2` variables. Sclaing not possible.", call. = FALSE)
-  }else{
-    if(x_len >= 2 && x_len <= 3){
-      warning("`x` has  `< 4`. Scaling possible but not recommended.",
-              call. = FALSE)
-    }else{
-      if(x_len > 3){
-        msg <- paste("`x` has", x_len, "variables")
-        message(msg)
-      }
+    stop("`x` has less than 2 variables.")
     }
-  }
   x_nms <- names(x)
   x_unms <- unique(x_nms)
   if(is.null(names)){
-    stop("`x` has no (col)names. Scaling not possible.", call. = FALSE)
+    stop("`x` has no (col)names.", call. = FALSE)
   }else{
-    if(length(x_nms) > length(x_unms)){
-    warning ("`x` names are not unique. Scaling possible but not recommended.",
-             call. = FALSE)
-    }else{
-      message("`x` names are unique.")
+    if(!identical(x_nms, x_unms)) {
+    warning ("`x` names are not unique.", call. = FALSE)
     }
     if(anyNA(x_nms)){
-      warning ("`x` has `NA` names. Scaling possible but not recommended.",
-               call. = FALSE)
-    }else{
-      message("`x` has no `NA` names.")
+      warning ("`x` has `NA` names.", call. = FALSE)
     }
   }
-  message("Ready to go!")
+  df_nme <- match.call()$x
+  msg <- paste0("`" , df_nme, "`", " fits the bill. Ready to scale! ")
+  message(msg)
 }
