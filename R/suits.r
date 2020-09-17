6cf13572b1288c7c51e87e6193749f5155b5efaf
Steven Bißantz <steven.bissantz@mail.de>
2020-09-17 23:39:26 +0200

    changed error messages

--- R/suits.r
+++ R/suits.r
@@ -1,11 +1,23 @@
 suits <- function(df) {
   tryCatch(check_my(df),
            error = function(e) {
-             #errmsg <- conditionMessage(e)
-             errmsg <- "Scaling not possible"
-             stop(errmsg, call. = FALSE)
+             cmsg <- conditionMessage(e)
+             advice <- "Scaling not possible!"
+             hint <- "\n Hint: Fix this error and retry. \n"
+             errmsg <- paste(advice, cmsg, hint)
+              stop(errmsg, call. = FALSE)
+           },
+           warning = function(w){
+             cmsg <- conditionMessage(w)
+             advice <- "Scaling not recommended \n"
+             hint <- "Hint: Investigate in this warning and retry. \n"
+             wrnmsg <- paste(cmsg, advice, hint)
+              warning(wrnmsg, call. = FALSE)
            }
-           )
+        )
+  invisible(structure(df, checks = TRUE))
   }
 
-
+#
+# Attaches an Attr  "checks = TRUE" and messages if data.frame is not checked?
+#
