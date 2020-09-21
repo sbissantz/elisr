1725015879086f722df136a2ac0bdbf840155e05
Steven Bißantz <steven.bissantz@mail.de>
2020-09-21 11:16:46 +0200

    fixed muscldf with parent.frame()

---
+++ R/new_muscldf.r
@@ -0,0 +1,17 @@
+# TODO Ordering of Arguments
+
+# Constructor -------------------------------------------------------------
+
+new_muscldf <- function(x = list(), method = "disjoint", rit_min = NULL,
+                        negative_too = FALSE, sclvals = NULL, df = NULL) {
+  stopifnot(exprs = {
+    is.list(x)
+    is.double(rit_min) || is.null(rit_min)
+    is.logical(negative_too)
+    is.vector(sclvals) || is.null(sclvals)
+    is.data.frame(eval(df, parent.frame()))
+  })
+  method <- match.arg(method, c("disjoint", "overlap"))
+  structure(x, class = "muscldf", method = method, rit_min = rit_min,
+            negative_too = negative_too, sclvals = sclvals, df = df)
+}
