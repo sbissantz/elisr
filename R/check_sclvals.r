5aeb383b6f75da8a6062a11f4f15ff1932e06455
Steven Bißantz <steven.bissantz@mail.de>
2020-09-21 18:24:28 +0200

    function to check scale values

---
+++ R/check_sclvals.r
@@ -0,0 +1,14 @@
+# sclvals shouldn't be...
+# TODO NULL
+# TODO length 2
+# check_sclvals -----------------------------------------------------------
+
+check_sclvals <- function(scale_values){
+  if (is.null(sclvals))
+    stop("No vector of 'sclvals' found. Please specify one.", call. = FALSE)
+  if (!is.vector(sclvales)){
+  }
+
+
+}
+
