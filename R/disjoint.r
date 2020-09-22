b67578353bf7db35181b6fb67ae6faa79b00ef37
Steven Bißantz <steven.bissantz@mail.de>
2020-09-22 15:19:44 +0200

    integrated disj_nci instead of disunito

--- R/disjoint.r
+++ R/disjoint.r
@@ -1,10 +1,13 @@
+# TODO muscldf -> muscls
+# TODO check `...`
+# TODO check that all "''" are "``"
 
 # disjoint ----------------------------------------------------------------
 
 disjoint <- function(df, rit_min = .3, negative_too = FALSE,
                      sclvals = NULL, ...) {
   if (isTRUE(negative_too)){
-    scls <- disunito(df, rit_min, sclvals)
+    scls <- disj_nci(df, rit_min, sclvals)
     new_muscldf(scls, df = match.call()$df, method = "disjoint",
                 rit_min = rit_min, negative_too = TRUE, sclvals = sclvals)
     }else{
