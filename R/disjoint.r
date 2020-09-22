a8a0554e0aa5cf9f1614430eacdd20204485b29e
Steven Bißantz <steven.bissantz@mail.de>
2020-09-22 14:35:43 +0200

    changed lodis to scls

--- R/disjoint.r
+++ R/disjoint.r
@@ -4,12 +4,12 @@
 disjoint <- function(df, rit_min = .3, negative_too = FALSE,
                      sclvals = NULL, ...) {
   if (isTRUE(negative_too)){
-    lodis <- disunito(df, rit_min, sclvals)
-    new_muscldf(lodis, df = match.call()$df, method = "disjoint",
+    scls <- disunito(df, rit_min, sclvals)
+    new_muscldf(scls, df = match.call()$df, method = "disjoint",
                 rit_min = rit_min, negative_too = TRUE, sclvals = sclvals)
     }else{
-      lodis <- disupi(df, rit_min)
-      new_muscldf(lodis, df = match.call()$df, method = "disjoint",
+      scls <- disj_pci(df, rit_min)
+      new_muscldf(scls, df = match.call()$df, method = "disjoint",
                rit_min = rit_min, negative_too = FALSE, sclvals = sclvals)
   }
 }
