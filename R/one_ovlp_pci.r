3fde06cb01778b75d9dd210e6fc822fc9d54df20
Steven Bißantz <steven.bissantz@mail.de>
2020-09-22 14:36:14 +0200

    outsourced this helper fun

---
+++ R/one_ovlp_pci.r
@@ -0,0 +1,27 @@
+
+# One Overlap -- Positive Correlating Items -------------------------------
+
+one_ovlp_pci <- function(scl, ebscl, rit_min, ...) {
+   while (ncol(ebscl) >= 1) {
+     cormat <- cor(rowSums(scl), ebscl, ...)
+     maxcor <- max(cormat[cormat < 1])
+     if (maxcor < rit_min) break
+     fstmaxp <- which(cormat == maxcor)
+     scl <- cbind(scl, ebscl[fstmaxp])
+     ebscl <- ebscl[-fstmaxp]
+     }
+   return(scl)
+ }
+
+ #one_ovlp_pci <- function(ovls, wfls, rit_min, ...) {
+ #  while (ncol(wfls) >= 1) {
+ #    cormat <- cor(rowSums(ovls), wfls, ...)
+ #    maxcor <- max(cormat[cormat < 1])
+ #    if (maxcor < rit_min) break
+ #    fstmaxp <- which(cormat == maxcor)
+ #    ovls <- cbind(ovls, wfls[fstmaxp])
+ #    wfls <- wfls[-fstmaxp]
+ #    }
+ #  return(ovls)
+ #}
+
