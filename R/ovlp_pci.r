69661efc083250175dcadbe12592fc43d28eb407
Steven Bißantz <steven.bissantz@mail.de>
2020-09-22 14:36:33 +0200

    renamed ovlupi

---
+++ R/ovlp_pci.r
@@ -0,0 +1,48 @@
+
+# Overlapping Scaling Process Using Positive Items ------------------------
+
+# ovlupi takes a multiple sclaed data.frame and a lower bound in form of a
+# rit_min. By choosing the items used in the overlapping process (using only the
+# highest correlating pair of each scale, i.e. the core, or trying to overlap
+# or expand the 'full_scale'). No matter But whatever which one one chooses it
+# returns again a (list of) multiple scaled data.frame(s). Ellipsis is set to
+# allow for any na.action(), Hint: One could also set the 'method' argument from
+# cor() but this is not tested yet.
+
+ovlp_pci <- function(muscldf, rit_min = NULL, overlap_with = NULL, ...) {
+
+# checks ------------------------------------------------------------------
+
+  if (isFALSE(inherits(muscldf, "muscldf")))
+    stop("This is not a muscldf. Please build on", call. = FALSE)
+  if (is.null(overlap_with))
+    stop("No method to 'overlap_with'. Please specify one.", call. = FALSE)
+  #units <- match.arg(units, c("secs", "mins", "hours", "days", "weeks"))
+  #if (isFALSE(attr(muscldf, "colnames")))
+  #  stop("Colnames seemd to be lost. Please search for them.", call. = FALSE)
+
+# pre-defined values -----------------------------------------------------
+
+  df <- eval(attr(muscldf, "df"))
+  if (is.null(rit_min)) {
+    rit_min <- attr(muscldf, "rit_min")
+  }
+  scl_nms <- lapply(muscldf, names)
+  nuc_nms <- lapply(scl_nms, function(scl_nms) scl_nms[c(1, 2)])
+
+# options -----------------------------------------------------------------
+
+  switch(overlap_with,
+         full_scale = {
+          scls <- Map(extr_itms, muscldf, scl_nms)
+          ebscls <- Map(extreb_itms, muscldf, scl_nms)
+         },
+         core = {
+          scls <- Map(extr_itms, muscldf, nuc_nms)
+          ebscls <- Map(extreb_itms, muscldf, nuc_nms)
+         },
+         stop("Unknown overlapping method. Use either `core` or `full_scale`",
+              call. = FALSE)
+         )
+  Map(one_ovlp_pci, scls, ebscls, rit_min)
+}
