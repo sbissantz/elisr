a4e17107ae2c5338b0e0fb58173f309dd08e501f
Steven Bißantz <steven.bissantz@mail.de>
2020-09-24 18:06:01 +0200

    reintroduced suits

--- R/suits.r
+++ R/suits.r
@@ -2,22 +2,22 @@ suits <- function(df, reuse = TRUE) {
   reuse_logi <- is.logical(reuse)
   if(isFALSE(reuse_logi))
     stop("`reuse` must be logical (i.e., either TRUE or FALSE)", call. = FALSE)
-  tryCatch(check_my(df),
+  tryCatch(check_df(df),
            error = function(e) {
              cmsg <- conditionMessage(e)
              advice <- "Scaling not possible!"
              hint <- "\n Hint: Fix this error and retry. \n"
              errmsg <- paste(advice, cmsg, hint)
-              stop(errmsg, call. = FALSE)
+             stop(errmsg, call. = FALSE)
            },
            warning = function(w){
              cmsg <- conditionMessage(w)
              advice <- "Scaling not recommended \n"
              hint <- "Hint: Investigate in this warning and retry. \n"
              wrnmsg <- paste(cmsg, advice, hint)
-              warning(wrnmsg, call. = FALSE)
+             warning(wrnmsg, call. = FALSE)
            }
-        )
+  )
   df_nme <- match.call()$df
   msg <- paste0("`" , df_nme, "`", " fits the bill. Ready to scale! ")
   message(msg)
