364eb80654aeeeba77fc8eea39ff1608a0cca10b
Steven Bißantz <steven.bissantz@mail.de>
2020-09-24 18:03:44 +0200

    seperate checks fpr scalevals and adjusted new_muscldf

--- R/disjoint.r
+++ R/disjoint.r
@@ -7,12 +7,13 @@
 disjoint <- function(df, rit_min = .3, negative_too = FALSE,
                      sclvals = NULL, ...) {
   if (isTRUE(negative_too)){
+    # check_sclvals(sclvals)
     scls <- disj_nci(df, rit_min, sclvals)
     new_muscldf(scls, df = match.call()$df, method = "disjoint",
                 rit_min = rit_min, negative_too = TRUE, sclvals = sclvals)
     }else{
       scls <- disj_pci(df, rit_min)
       new_muscldf(scls, df = match.call()$df, method = "disjoint",
-               rit_min = rit_min, negative_too = FALSE, sclvals = sclvals)
+               rit_min = rit_min, negative_too = FALSE)
   }
 }
