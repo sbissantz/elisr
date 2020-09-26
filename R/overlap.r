09778d5bdf4353dd9ad72098abeb9a85d5514ad5
Steven Bißantz <steven.bissantz@mail.de>
2020-09-26 07:31:56 +0200

    integrated checks with FUNS

--- R/overlap.r
+++ R/overlap.r
@@ -1,27 +1,32 @@
-# TODO muscldf -> muscls
+# -------------------------------------------------------------------------
+
 # TODO check `...`
-# TODO check that all "''" are "``"
+# TODO muscldf -> muscls
 # TODO mak overlap_with -> method, olap_method
 # TODO negative_too -> change
-# TODO stopifnot( negative_too) is either TRUE or FALSE, i.e. exclude NULL (input must be logical)
 
 overlap <- function(muscldf, rit_min = .3, negative_too = FALSE,
-                    overlap_with = "full_scale", sclvals = NULL, ...){
+                    overlap_with = "full_scale", sclvals = NULL, ...) {
 
-  if (isFALSE(inherits(muscldf, "muscldf")))
-    stop("`muscldf` is not of its type. Consider `disjoint()` to create one.",
-         call. = FALSE)
-  if (is.null(overlap_with))
-    stop("No method to 'overlap_with'. Please specify one.", call. = FALSE)
+# Checks ------------------------------------------------------------------
+
+  check_neg(negative_too)
+  check_muscldf(muscldf)
+  check_rit(rit_min)
+  check_neg(negative_too)
+  check_ovlp(overlap_with)
+
+# Procedures --------------------------------------------------------------
+
   if(negative_too){
-    # check_sclvals(sclvals)
+    check_sclvals(sclvals)
     scls <- ovlp_nci(muscldf, rit_min, overlap_with, sclvals, ...)
     new_muscldf(scls, df = attr(muscldf, "df"), method = "overlap",
-                rit_min = rit_min, negative_too = TRUE, sclvals = attr(muscldf, "sclvals"))
+                rit_min = rit_min, negative_too = TRUE,
+                sclvals = attr(muscldf, "sclvals"))
   }else{
     scls <- ovlp_pci(muscldf, rit_min, overlap_with, sclvals, ...)
     new_muscldf(scls, df = attr(muscldf, "df"), method = "overlap",
                 rit_min = rit_min, negative_too = FALSE)
   }
 }
-
