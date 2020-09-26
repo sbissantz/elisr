96b06a53a1b203ed2edb71052c7e025fd00d767f
Steven Bißantz <steven.bissantz@mail.de>
2020-09-26 07:29:09 +0200

    use check_*() instead of manually specifiying options

--- R/disjoint.r
+++ R/disjoint.r
@@ -6,8 +6,17 @@
 
 disjoint <- function(df, rit_min = .3, negative_too = FALSE,
                      sclvals = NULL, ...) {
-  if (isTRUE(negative_too)){
-    # check_sclvals(sclvals)
+
+# Checks ------------------------------------------------------------------
+
+  check_neg(negative_too)
+  check_df(df)
+  check_rit(rit_min)
+
+# Procedures --------------------------------------------------------------
+
+  if (negative_too){
+    check_sclvals(sclvals)
     scls <- disj_nci(df, rit_min, sclvals)
     new_muscldf(scls, df = match.call()$df, method = "disjoint",
                 rit_min = rit_min, negative_too = TRUE, sclvals = sclvals)
@@ -17,3 +26,4 @@ disjoint <- function(df, rit_min = .3, negative_too = FALSE,
                rit_min = rit_min, negative_too = FALSE)
   }
 }
+
