ed0bdd08476b6c29fa3fd243856dc685e4a9685f
Steven Bißantz <steven.bissantz@mail.de>
2020-09-26 07:32:23 +0200

    sourced out manual checks

--- R/ovlp_nci.r
+++ R/ovlp_nci.r
@@ -1,26 +1,18 @@
 
 # Overlapping Scaling Process Using Negative Items Too --------------------
-# TODO muscldf -> muscls
+
 # TODO check `...`
-# TODO if someone hasnt disunito() before he hasn't specified a sclvals
 
 ovlp_nci <- function(muscldf, rit_min = NULL, overlap_with = NULL,
                      sclvals = NULL, ...) {
+# Pre-Sets ----------------------------------------------------------------
 
-  df_nme <- attr(muscldf, "df")
-  df <- eval(df_nme)
-  if(is.null(rit_min)){
-    rit_min <- attr(muscldf, "rit_min")
-  }
-  check_rit(rit_min)
-
-  if(is.null(sclvals)){
-  sclvals <- attr(muscldf, "sclvals")
-  }
-  check_sclvals(sclvals)
-
+  df <- eval(attr(muscldf, "df"))
   scl_nms <- lapply(muscldf, names)
   core_nms <- lapply(scl_nms, function(scl_nms) scl_nms[c(1, 2)])
+
+# Options -----------------------------------------------------------------
+
   switch(overlap_with,
          full_scale = {
            scls <- lapply(scl_nms, extr_itms, df = df)
