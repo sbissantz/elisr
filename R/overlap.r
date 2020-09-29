64f36331224cfcdc18005177a3db65ae34ba65c4
Steven Bißantz <steven.bissantz@mail.de>
2020-09-29 12:46:08 +0200

    integrated compare_sclvals()

--- R/overlap.r
+++ R/overlap.r
@@ -1,29 +1,37 @@
-# -------------------------------------------------------------------------
 
-# TODO check `...`
-# TODO negative_too -> change
+# Multiple Scaling In An Overlapping Manner -------------------------------
 
-overlap <- function(muscldf, rit_min = .3, negative_too = FALSE,
-                    overlap_with = "full_scale", sclvals = NULL, ...) {
+overlap <- function(muscldf, rit_min = .5, negative_too = FALSE,
+                    overlap_with = "full_scale", sclvals = NULL,
+                    use = "pairwise.complete.obs") {
 
 # Checks ------------------------------------------------------------------
+
+# TODO Implement a check that the sclval attribute and the input are identical
+# Only a match if sclvals are equal?
+# IF an attribute is set, THEN check, if the attribute is equal to the input.
+#
 
   check_neg(negative_too)
   check_muscldf(muscldf)
   check_rit(rit_min)
-  check_neg(negative_too)
   check_ovlp(overlap_with)
 
-# Procedures --------------------------------------------------------------
+# Procedure & Options -----------------------------------------------------
 
   if(negative_too){
+    sclvals_attr <- attr(muscldf, "sclvals")
+    compare_sclvals(sclvals, sclvals_attr)
+    if(is.null(sclvals)){
+      sclvals <- sclvals_attr
+    }
     check_sclvals(sclvals)
-    scls <- ovlp_nci(muscldf, rit_min, overlap_with, sclvals, ...)
+    scls <- ovlp_nci(muscldf, rit_min, overlap_with, sclvals, use = use)
     new_muscldf(scls, df = attr(muscldf, "df"), method = "overlap",
                 rit_min = rit_min, negative_too = TRUE,
-                sclvals = attr(muscldf, "sclvals"))
+                sclvals = sclvals)
   }else{
-    scls <- ovlp_pci(muscldf, rit_min, overlap_with, sclvals, ...)
+    scls <- ovlp_pci(muscldf, rit_min, overlap_with, use = use)
     new_muscldf(scls, df = attr(muscldf, "df"), method = "overlap",
                 rit_min = rit_min, negative_too = FALSE)
   }
