dae566b66d89e23b74f64193d16020fa3c4a72bd
Steven Bißantz <steven.bissantz@mail.de>
2020-09-29 12:43:38 +0200

    integrated the use ARG

--- R/disjoint.r
+++ R/disjoint.r
@@ -1,26 +1,24 @@
-# TODO muscldf -> muscls
-# TODO check `...`
 
-# disjoint ----------------------------------------------------------------
+# Multiple Scaing in A Disjoint Manner ------------------------------------
 
 disjoint <- function(df, rit_min = .3, negative_too = FALSE,
-                     sclvals = NULL, ...) {
+                     sclvals = NULL, use = "pairwise.complete.obs") {
 
 # Checks ------------------------------------------------------------------
 
-  check_neg(negative_too)
   check_df(df)
   check_rit(rit_min)
+  check_neg(negative_too)
 
-# Procedures --------------------------------------------------------------
+# Procedure & Options -----------------------------------------------------
 
   if (negative_too){
     check_sclvals(sclvals)
-    scls <- disj_nci(df, rit_min, sclvals)
+    scls <- disj_nci(df, rit_min, sclvals, use)
     new_muscldf(scls, df = match.call()$df, method = "disjoint",
                 rit_min = rit_min, negative_too = TRUE, sclvals = sclvals)
     }else{
-      scls <- disj_pci(df, rit_min)
+      scls <- disj_pci(df, rit_min, use)
       new_muscldf(scls, df = match.call()$df, method = "disjoint",
                rit_min = rit_min, negative_too = FALSE)
   }
