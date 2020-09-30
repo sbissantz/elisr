0a7187275af41884a4428c6aae32b014cf6b1a7a
Steven Bißantz <steven.bissantz@mail.de>
2020-09-30 10:31:10 +0200

    Updated the manpage

--- R/disjoint.r
+++ R/disjoint.r
@@ -1,13 +1,41 @@
-#' Multiple scaling in a disjoint manner
+#' @title Multiple scaling in a disjoint manner
 #'
-#' @param df a data frame.
-#' @param rit_min a corrected item total correlation between 0 and 1 marking the
-#'   lower bound of the scaling process.
+#' @description \code{disjoint} returns a multiple, disjoint scaled version of
+#'   the specified data frame. This so called \code{muscldf} is nothing more
+#'   than a list of scales (in this case genuine subsets of that data frame).
+#' @param df a data frame object.
+#'
+#' @param rit_min a numerical constant to specify the (corrected item total)
+#'   correlation. The value of this lower bound must in the range of 0 to 1.
+#'
 #' @param negative_too a logical constant indicating whether negatively
 #'   correlating items should be included in the scaling process.
-#' @param sclvals a double vector of the form \code{c(min,max)} marking the
-#'   start and end point of your scale, e.g.
 #'
+#' @param sclvals a numerical vector of length 2 indicating the first and the
+#'   full scale value. Consider using the shape \code{c(min,max)}.
+#'
+#' @param use an optional string indicating how to deal with missing values if
+#'   necessary. See \code{use} in \code{stats::cor()} for details.
+#'
+#' @details The \code{use} argument takes control over the treatment of missing
+#'   values when correlation matrices are build. In a scaling process this
+#'   happens at least twice: first when determining the core (the two items of
+#'   the correlation matrix with the highest linear relationship), and second
+#'   when an item is considered to be part of this scale.
+#'
+#' @references Müller-Schneider, Thomas. (2001). Multiple Skalierung nach dem
+#'   Kristallisationsprinzip / Multiple Scaling According to the Principle of
+#'   Crystallization. Zeitschrift für Soziologie. 30. 10.1515/zfsoz-2001-0404.
+#'
+#' @examples
+#' # Using positive correlations (and `pairwise.complete.obs`)
+#' disjoint(mtcars, rit_min = .4)
+#'
+#' # Including negative correlations (and `pairwise.complete.obs`)
+#' disjoint(mtcars, rit_min = .4, negative_too = TRUE, sclvals = c(1,7))
+#'
+#' # Changing the treatment of missing values
+#' disjoint(mtcars, rit_min = .4, use = "all.obs")
 
 disjoint <- function(df, rit_min = .3, negative_too = FALSE,
                      sclvals = NULL, use = "pairwise.complete.obs") {
