8b851e33dd020907f2f783b8b185325fc7e338db
Steven Bißantz <steven.bissantz@mail.de>
2020-11-16 14:04:56 +0100

    updated the man page

--- R/new_muscldf.r
+++ R/new_muscldf.r
@@ -1,11 +1,19 @@
-#'   This so called \code{muscldf} is nothing more
-#'   than a list of scales (in this case genuine subsets of that data frame).
+#' @title Creator Function to Build A New \code{muscldf}
 #'
-# Constructor -------------------------------------------------------------
-# TODO overlap_with not as Attrb?
+#' @description A \code{muscldf} is a multiple scaled data frame. A multiple
+#' scaled data frame is nothing more than a list of scales with attributes. A
+#' scale is just a subset of the specified data frame (e.g., \code{disjoint}) or
+#' an extension of the given \code{muscldf} (e.g., \code{overlap} -- were each
+#' scale is built according to the crystallization principle.
+#'
+#' @details The \code{muscldf} is only for internal use. A check function
+#' assures that the argument to \code{overlap} is of that type. Another useful
+#' feature is that the attributes provide information which settings were used
+#' in the scaling process -- after scaling has been done. So one can be sure
+#' that all parameters are set correctly.
 
 new_muscldf <- function(x = list(), method, rit_min,
-                        # sclvals must be set NULL -- asures
+                        # sclvals must be set NULL -- assures that
                         negative_too, sclvals = NULL,
                         df) {
   stopifnot(exprs = {
