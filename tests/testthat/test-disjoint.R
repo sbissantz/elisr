98a505e3914272ff93859b678f4b19e94a7a908b
Steven Bißantz <steven.bissantz@mail.de>
2020-12-09 11:36:11 +0100

    added test for nme_muscldf

--- tests/testthat/test-disjoint.R
+++ tests/testthat/test-disjoint.R
@@ -3,3 +3,11 @@ context("Test disjoint")
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+
+
+test_that("name muscldf's", {
+  res_nme <- nme_muscldf(2)
+  expect_equal(res_nme, "scl_1")
+})
+
