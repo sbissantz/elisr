f7adcc3ab37821fa1a3dfa738c6870cac01e804b
Steven Bißantz <steven.bissantz@mail.de>
2020-12-18 13:14:10 +0100

    assigned scl_nms earlier in the process

--- R/new_muscldf.r
+++ R/new_muscldf.r
@@ -24,6 +24,7 @@ new_muscldf <- function(x = list(), method, rit_min,
     is.data.frame(eval(df, parent.frame()))
   })
   method <- match.arg(method, c("disjoint", "overlap"))
+  scls_nms <- nme_muscldf(x)
   # Should do the trick
   if (is.null(sclvals)) {
     sclvals <- (attr(sclvals, "sclvals"))
@@ -32,5 +33,6 @@ new_muscldf <- function(x = list(), method, rit_min,
   }
   }
   structure(x, class = "muscldf", method = method, rit_min = rit_min,
-            negative_too = negative_too, sclvals = sclvals, df = df)
+            negative_too = negative_too, sclvals = sclvals, df = df,
+            names = scls_nms)
 }
