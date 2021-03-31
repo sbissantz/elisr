c43d84cdb0e9db92e456c1e8e7cec835b88da121
Steven Bißantz <steven.bissantz@mail.de>
2021-03-31 11:04:57 +0200

    documented is.msdf

--- R/is.msdf.R
+++ R/is.msdf.R
@@ -1,3 +1,11 @@
-# document
+#' @title Test for a multiple scaled data frame object
+#'
+#' @description \code{is.msdf} tests if the given object is of type \code{msdf}.
+#'
+#' @param x an arbitrary object
+#'
+#' @details \code{is.msdf} returns \code{TRUE} if the given object is of type
+#'   \code{msdf} and FALSE otherwise.
+
 #'@export
 is.msdf <- function(x) inherits(x, "msdf")
