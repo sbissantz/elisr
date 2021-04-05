b3affaea0f4eeb7e4c3c9d5d0073e03394bbd90e
Steven Bißantz <steven.bissantz@mail.de>
2021-04-05 19:59:40 +0200

    typos

--- R/trust.R
+++ R/trust.R
@@ -1,7 +1,7 @@
 #' @title German General Social Survey's Trust Items
 #'
 #' @description A subset of the German General Social Survey (ALLBUS) 2018
-#'   containing its `trust items` (F018).
+#'   containing its trust items (F018).
 #'
 #' @format Participants were asked about their trust in public institutions and
 #'   organizations. `1` means they have absolutely no trust at all, whereas `7`
@@ -23,16 +23,16 @@
 #'   \item{eupalmnt}{Europen Parliament}
 #'   }
 #'
-#' @details Please note that, in comparison to the original data set,
-#'   participant order has been randomly rearranged to further ensure anonymity.
-#'   This might lead to differences when trying to reproduce the results of the
-#'   analysis with the original data set, mentioned below.
+#' @details Please note that in comparison to the original data set participant
+#'   order has been randomly rearranged to further ensure anonymity. This might
+#'   lead to differences when trying to reproduce the results of the analysis
+#'   with the original data set (mentioned below).
 #'
 #' @references{ GESIS - Leibniz-Institut für Sozialwissenschaften (2019). German
 #' General Social Survey - ALLBUS 2018. GESIS Datenarchiv, Köln. ZA5272
 #' Datenfile Version 1.0.0, https://doi.org/10.4232/1.13325.}
 #'
 #' @examples
-#' #
-#' disjoint(mtcars, mrit_min = .4)
+#' # Use trust with disjoint
+#' disjoint(trust, mrit_min = .4)
 "trust"
