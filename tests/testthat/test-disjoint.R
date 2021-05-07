d9d4178c13a567079c838bbc000a4ec971a5e164
Steven Bißantz <steven.biisantz@gmail.com>
2021-05-07 16:40:36 +0200

    new tests

--- tests/testthat/test-disjoint.R
+++ tests/testthat/test-disjoint.R
@@ -1,17 +1,14 @@
 test_that("positive scaling", {
    msdf <- disjoint(trust, mrit_min = .55)
-   eupalmnt <- trust[, "eupalmnt"]
-   expect_equal(length(msdf), 2)
-   expect_equal(length(msdf$scl_1), 7)
-   expect_equal(length(msdf$scl_2), 2)
-   expect_equal(msdf$scl_1[, 1], eupalmnt)
+   expect_length(msdf, 2)
+   expect_length(msdf$scl_1, 7)
+   expect_length(msdf$scl_2, 2)
 })
 
 test_that("negative scaling", {
    msdf <- disjoint(trust, mrit_min = .55)
-   msdf_new <- disjoint(df = {
-     trust$polpati <- (1 + 7) - trust[, "polpati"]
-     trust
-   }, mrit_min = .55, negative_too = TRUE, sclvals = c(1, 7))
-   expect_equal(msdf$scl_1[, "polpati"], msdf_new$scl_1[, "polpati"])
+   msdf_rev <- disjoint(
+      within(trust, polpati <- 8 - polpati),
+      mrit_min = .55, negative_too = TRUE, sclvals = c(1, 7))
+   expect_setequal(msdf$scl_1[, "polpati"],  msdf_rev$scl_1[, "polpati"])
 })
--- tests/testthat/test-overlap.R
+++ tests/testthat/test-overlap.R
@@ -1,58 +1,52 @@
 test_that("positive scaling", {
-   msdf_d <- disjoint(trust, mrit_min = .55)
-   (msdf_o <- overlap(msdf_d, mrit_min = .3))
-   eupalmnt <- trust[, "eupalmnt"]
-   expect_equal(length(msdf_o), 2)
-   expect_equal(length(msdf_o$scl_1), 13)
-   expect_equal(length(msdf_o$scl_2), 13)
-   expect_equal(msdf_o$scl_1[, 1], eupalmnt)
+   msdf <- overlap(disjoint(trust, mrit_min = .55),
+                   mrit_min = .3)
+   expect_length(msdf, 2)
+   expect_length(msdf$scl_1, 13)
+   expect_length(msdf$scl_2, 13)
 })
 
 test_that("attributes inherits", {
-   msdf_d <- disjoint(trust, mrit_min = .55, negative_too = TRUE,
-                      sclvals = c(1, 7))
-   msdf_o <- overlap(msdf_d, negative_too = TRUE)
-   expect_equal(attr(msdf_o, "mrit_min"), .55)
-   expect_equal(attr(msdf_o, "sclvals"), c(1, 7))
+   msdf <- overlap(disjoint(trust, mrit_min = .55, negative_too = TRUE,
+                      sclvals = c(1, 7)), negative_too = TRUE)
+   expect_equal(attr(msdf, "mrit_min"), .55)
+   expect_setequal(attr(msdf, "sclvals"), c(1, 7))
 })
 
 test_that("core option", {
-   msdf_d <- disjoint(trust, mrit_min = .55)
-   msdf_o <- overlap(msdf_d, mrit_min = .3, overlap_with = "core")
+   msdf <- overlap(disjoint(trust, mrit_min = .55),
+                     mrit_min = .3, overlap_with = "core")
    eupalmnt <- trust[, "eupalmnt"]
-   expect_equal(length(msdf_o), 2)
-   expect_equal(length(msdf_o$scl_1), 13)
-   expect_equal(length(msdf_o$scl_2), 13)
-   expect_equal(msdf_o$scl_1[, 1], eupalmnt)
+   expect_length(msdf, 2)
+   expect_length(msdf$scl_1, 13)
+   expect_length(msdf$scl_2, 13)
 })
 
 test_that("overlap_with metod", {
-   msdf_d <- disjoint(trust)
-   expect_error(overlap(msdf_d, overlap_with = "me"))
-   expect_error(overlap(msdf_d, negative_too = TRUE, sclvals = c(1, 7),
-                        overlap_with = "me"))
+   msdf <- disjoint(trust)
+   expect_error(overlap(msdf, overlap_with = "test_1"))
+   expect_error(overlap(msdf, negative_too = TRUE, sclvals = c(1, 7),
+                        overlap_with = "test_2"))
 })
 
 test_that("reversed items", {
-   mdf <- disjoint(trust, mrit_min = .55)
-   msdf <- overlap(mdf, mrit_min = .55)
-   trust$fedgovt <- (1 + 7) - trust[, "fedgovt"]
-   mdf_new <- disjoint(trust, mrit_min = .55)
-   msdf_new <- overlap(mdf_new, mrit_min = .3, negative_too = TRUE,
-                       sclvals = c(1, 7))
-   expect_equal(msdf$scl_1[, "eupalmnt"], msdf_new$scl_1[, "eupalmnt"])
-   expect_equal(msdf$scl_1[, "fedgovt"], msdf_new$scl_1[, "fedgovt"])
+   msdf <- overlap(disjoint(trust, mrit_min = .55))
+   msdf_rev <- overlap(disjoint(within(trust, fedgovt <- 8 - fedgovt),
+                                mrit_min = .55), negative_too = TRUE,
+                                sclvals = c(1, 7))
+   expect_setequal(msdf$scl_1[, "eupalmnt"], msdf_rev$scl_1[, "eupalmnt"])
+   expect_setequal(msdf$scl_1[, "fedgovt"], msdf_rev$scl_1[, "fedgovt"])
 })
 
 test_that("core option + reversed items", {
-   trust$eupalmnt <- (1 + 7) - trust[, "eupalmnt"]
-   trust$fedgovt <- (1 + 7) - trust[, "fedgovt"]
-   msdf_d <- disjoint(trust, mrit_min = .55)
-   msdf_o <- overlap(msdf_d, mrit_min = .3, overlap_with = "core",
-                      negative_too = TRUE, sclvals = c(1, 7))
-   expect_equal(length(msdf_o), 3)
-   expect_equal(length(msdf_o$scl_1), 13)
-   expect_equal(length(msdf_o$scl_2), 13)
-   expect_equal(length(msdf_o$scl_3), 13)
-   expect_equal(colnames(msdf_o$scl_1[1]), "newsppr")
+   msdf <- overlap(
+      disjoint(
+         within(trust, {eupalmnt <- 8 - eupalmnt; fedgovt <- 8 - fedgovt}),
+         mrit_min = .55), mrit_min = .3, overlap_with = "core",
+      negative_too = TRUE, sclvals = c(1, 7))
+   expect_length(msdf, 3)
+   expect_length(msdf$scl_1, 13)
+   expect_length(msdf$scl_2, 13)
+   expect_length(msdf$scl_3, 13)
+   expect_named(msdf$scl_1, colnames(trust), ignore.order = TRUE)
 })
